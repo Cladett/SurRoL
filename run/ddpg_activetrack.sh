375e9daec39b61ab858d5b5232a428f7d52c8b99
Claudia <c.dettorre@ucl.ac.uk>
2021-09-27 08:57:46 +0100

    Changing running script for reach needle env.

--- run/ddpg_activetrack.sh
+++ run/ddpg_activetrack.sh
@@ -7,5 +7,5 @@
 #python -m baselines.run --num_env=2 --alg=ddpg --env=ActiveTrack-v0 --num_timesteps=1e5 --save_video_interval=1 \
 #--log_path=../logs/ddpg/ActiveTrack-1e5_0
 
-CUDA_VISIBLE_DEVICES=2 python -m baselines.run --num_env=2 --env_type=custom_type --alg=ddpg --env=ActiveTrack-v0 --num_timesteps=1e5 
+CUDA_VISIBLE_DEVICES=2 python -m baselines.run --num_env=2 --env_type=custom_type --alg=ddpg --env=NeedleReach-v0 --num_timesteps=1e5 
 
