bb3c6a4804145eb3f33399b680c3b89623ad5960
Claudia <c.dettorre@ucl.ac.uk>
2021-09-27 06:47:43 +0100

    Adding last modifications.

--- run/ddpg_activetrack.sh
+++ run/ddpg_activetrack.sh
@@ -1,2 +1,10 @@
-python -m baselines.run --num_env=2 --alg=ddpg --env=ActiveTrack-v0 --num_timesteps=1e5 --save_video_interval=1 \
---log_path=../logs/ddpg/ActiveTrack-1e5_0
+#!/bin/bash
+#
+# @author: Claudia D'Ettorre 
+# @date: 20 Sep 2021 
+# @brief: testing the run of an env
+
+#python -m baselines.run --num_env=2 --alg=ddpg --env=ActiveTrack-v0 --num_timesteps=1e5 --save_video_interval=1 \
+#--log_path=../logs/ddpg/ActiveTrack-1e5_0
+
+python -m baselines.run --num_env=2 --env_type=custom_type --alg=ddpg --env=ActiveTrack-v0 --num_timesteps=1e5 
