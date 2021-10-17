7f5ea5e23d78c91c327f033a084ab1fe5f4b3d27
Claudia <c.dettorre@ucl.ac.uk>
2021-10-17 11:17:05 +0100

    Removing testing files.

--- run/E4_her_needlereach_dgx.sh
+++ run/E4_her_needlereach_dgx.sh
@@ -5,10 +5,10 @@
 # @brief: testing the run of an env using demos 
 
 # Seed 1
-python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her1 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her1 
+CUDA_VISIBLE_DEVICES=0 python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her1 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her1 
 # Seed 2
-python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her2 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her2 
+CUDA_VISIBLE_DEVICES=0 python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her2 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her2 
 # Seed 3
-python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her3 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her3 
+CUDA_VISIBLE_DEVICES=0 python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her3 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her3 
 # Seed 4
-python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her4 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her4 
+CUDA_VISIBLE_DEVICES=0 python -m baselines.run --num_env=2 --alg=her --env=NeedleReach-v0 --num_timesteps=1e5 --save_path=/home/cladet/workspace/SurRoL/experiments/policies/NeedleReach-1e5_1_her4 --log_path=/home/cladet/workspace/SurRoL/experiments/logs/NeedleReach-1e5_1_her4 
